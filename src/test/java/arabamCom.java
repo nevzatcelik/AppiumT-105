b35b645dba9cf6fc87c7228a6d9c87f3d0602109
Nevzat <nevzatcelik151907@gmail.com>
2023-04-12 23:01:16 +0200

    Third Commit

--- src/test/java/arabamCom.java
+++ src/test/java/arabamCom.java
@@ -1,13 +1,19 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class arabamCom {
@@ -29,6 +35,7 @@ public class arabamCom {
 // aracimizin fiyatinin 500.000 tl den fazla oldugunu test edelim
 // uygulamayi kapatalim
 AppiumDriver<MobileElement> driver;
+
 
 
     @BeforeTest
@@ -54,6 +61,70 @@ AppiumDriver<MobileElement> driver;
     public void test01(){
         driver.activateApp("com.dogan.arabam");
 
+// Arabam kac para bolumune tiklayalim
+        MobileElement arabamKacPara=driver.findElement(By.id("com.dogan.arabam:id/tvPricePrediction"));
+        arabamKacPara.click();
+// Aracimin fiyatini merak ediyorum bolumunetiklayalim
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView[1]/android.view.ViewGroup[3]/android.widget.TextView[1]")).click();
+// Wolkswagen markasini secelim
+        TouchAction action=new TouchAction<>(driver);
+        action.press(PointOption.point(551,1677))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(500)))
+                .moveTo(PointOption.point(538,407)).release().perform();
+
+       /* action.press(PointOption.point(538,407))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(500)))
+                .moveTo(PointOption.point(551,1677)).release().perform();
+
+                yani yukari dogru haraket etmek istersek de koordinatlar uzerinden yukardaki islemin tam tersi koordinatlarini
+                girerek yukari kaydirma islemini gerceklestirmis oluruz
+        */
+        driver.findElement(By.xpath("//*[@text='Volkswagen']")).click();
+// yil secimi yapalim
+     driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[12]")).click();
+// model secimi yapalim
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[8]")).click();
+
+        // govde tipini secelim
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]")).click();
+
+// yakit tipini secelim
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.TextView")).click();
+// vites tipini secelim
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[3]")).click();
+
+// Versiyon secimi yapalim
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.FrameLayout[4]/android.view.ViewGroup/android.widget.LinearLayout/android.widget.TextView")).click();
+
+// aracin km bilgilerini girelim
+        driver.findElement(By.id("com.dogan.arabam:id/et_km")).sendKeys("190000");
+        driver.findElement(By.id("com.dogan.arabam:id/btn_price_prediction_submit")).click();
+// aracin rengini secelim
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[9]")).click();
+// opsiyel donanim (varsa) seecelim
+        driver.findElement(By.id("com.dogan.arabam:id/btnNext")).click();
+// degisen bilgisi ekleyerek tramer kaydi belirtelim
+        driver.findElement(By.id("com.dogan.arabam:id/iv_B0501")).click();
+        MobileElement sagOnKapi= driver.findElement(By.id("com.dogan.arabam:id/tvTitle"));
+        String sagOnKapiCheck=sagOnKapi.getText();
+        Assert.assertTrue(sagOnKapiCheck.contains("Sağ Ön Kapı"),"Sectiginiz kisim sag on kapi degil");
+        driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.LinearLayout/androidx.recyclerview.widget.RecyclerView/android.widget.RelativeLayout[3]/android.widget.TextView")).click();
+       driver.findElement(By.id("com.dogan.arabam:id/cardView")).click();
+       driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.widget.RadioGroup/android.widget.RadioButton[2]")).click();
+        driver.findElement(By.id("com.dogan.arabam:id/btnNext")).click();
+
+// aracimizin fiyatinin 500.000 tl den fazla oldugunu test edelim
+   MobileElement aracFiyati=  driver.findElement(By.id("com.dogan.arabam:id/tvAveragePrice"));
+   ///hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.view.ViewGroup[1]/android.widget.TextView[2]
+   String aracFiyatiCheck=aracFiyati.getText();
+        System.out.println(aracFiyatiCheck);
+   aracFiyatiCheck=aracFiyatiCheck.replaceAll(" TL","").replaceAll("\\D","");
+        System.out.println(aracFiyatiCheck);
+   Assert.assertTrue(Integer.parseInt(aracFiyatiCheck)>500000);
+
+           // 595000
+// uygulamayi kapatalim
+        driver.closeApp();
 
     }
 }
